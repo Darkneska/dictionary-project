23198d3d0133671457c836a1411898bc952e6c9c
Darkneska <84958830+Darkneska@users.noreply.github.com>
2021-09-27 18:21:09 +0200

    set dictionary + added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <header className="App-header">
       <img src="https://i.pinimg.com/originals/95/1a/5c/951a5cec4316bdc23b41adb6428d2284.gif" alt="main"></img>
       <h1>Search engine</h1>
+      <Dictionary />
      </header>
+     <footer><a
+        href="https://github.com/Darkneska/dictionary-project"
+        target="_blank"
+        rel="noreferrer"
+        className="source"
+      >
+        Open-sourced on GitHub
+      </a>{" "}
+      by Denisa Pavlikova{" "}
+      <a
+        href="https://cranky-engelbart-c691eb.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+        className="source"
+      >
+        hosted on Netlify
+      </a></footer>
+    </div>
     </div>
   );
 }
