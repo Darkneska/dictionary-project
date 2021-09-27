27fffbc1afd88aef8a95a48331d3d97e63d1dfab
Darkneska <84958830+Darkneska@users.noreply.github.com>
2021-09-27 17:46:41 +0200

    creating react + insert img + basic engine

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     <header className="App-header">
+      <img src="https://i.pinimg.com/originals/95/1a/5c/951a5cec4316bdc23b41adb6428d2284.gif" alt="main"></img>
+      <h1>Search engine</h1>
+     </header>
+    </div>
+  );
+}
+
+
